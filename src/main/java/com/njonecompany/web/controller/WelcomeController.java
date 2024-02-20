15d385074d5e0c55f8a399880ebd7e9f5ead86be
BoHyeon Sim <76395829+scofe97@users.noreply.github.com>
2024-02-21 00:22:53 +0900

    Update WelcomeController.java

--- src/main/java/com/njonecompany/web/controller/WelcomeController.java
+++ src/main/java/com/njonecompany/web/controller/WelcomeController.java
@@ -16,7 +16,7 @@ public class WelcomeController {
     @GetMapping("/")
     public String index(Model model) {
         // logger.debug("Welcome to njonecompany.com...");
-        System.out.prinlnkt(x: "index is called by Get /");
+        System.out.prinln(x: "index is called by Get /");
 
         model.addAttribute("msg", getMessage());
         model.addAttribute("today", new Date());
